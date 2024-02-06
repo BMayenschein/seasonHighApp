0beb3e762a240342ab670901fec6a4af5c4d0db6
Bruce Mayenschein <brucedmayenschein@gmail.com>
2024-02-06 09:05:41 -0500

    Added teams state functionality. User will be able to select which teams graphs to show

--- client/Season High/src/App.jsx
+++ client/Season High/src/App.jsx
@@ -14,6 +14,8 @@ import { Data } from '../utils/Data'
 import { LineChart } from './components/LineChart';
 
 
+import Teams from './components/Teams'
+
 
 ChartJS.register(
   CategoryScale,
@@ -26,64 +28,76 @@ ChartJS.register(
 );
 
 function App() {
-  const team = "heat"
-  const [options, setOptions] = useState(
-    {
-    responsive: true,
-    maintainAspectRatio: false,
-    title: {
-      display: true,
-      text: `Season High Scoring vs the ${team}`
-    },
-    elements: {
-      point: {
-        radius: 15,
-        pointStyle: Data[team].map((data) => {
-          const pointImage = new Image(35, 50);
-          const last = data.player.last_name.length > 5 ? data.player.last_name.slice(0, 5).toLowerCase() : data.player.last_name.toLowerCase() 
-          const first = data.player.first_name.slice(0,2).toLowerCase()
-          pointImage.src = `https://www.basketball-reference.com/req/202106291/images/headshots/jordami01.jpg`
-          return pointImage
-        })
-      }
-    },
-    scales: {
-      y: 
-        {
-          suggestedMin: 0,
-          suggestedMax: 80,
-          grid: {
-            color: '#ffc0cb',
-            tickColor: '#ffc0cb',
-            drawTicks: true,
-          },
-          ticks: {
-            backdropColor: '#ffc0cb'
-          }
-        },
-      x: {
-        grid: {
-          color: '#ffc0cb'
-        }
-      }
+  const [teams, setTeams] = useState([])
+
+  const handleTeams = (team) => {
+    if (teams.includes(team)) {
+      const newTeams = teams.filter(x => x !== team)
+      setTeams(newTeams)
     }
-  })
+    else {
+      setTeams([...teams, team])
+    }
+  }
+
+  // const [options, setOptions] = useState(
+  //   {
+  //   responsive: true,
+  //   maintainAspectRatio: false,
+  //   title: {
+  //     display: true,
+  //     text: `Season High Scoring vs the ${team}`
+  //   },
+  //   elements: {
+  //     point: {
+  //       radius: 15,
+  //       pointStyle: Data[team].map((data) => {
+  //         const pointImage = new Image(35, 50);
+  //         const last = data.player.last_name.length > 5 ? data.player.last_name.slice(0, 5).toLowerCase() : data.player.last_name.toLowerCase() 
+  //         const first = data.player.first_name.slice(0,2).toLowerCase()
+  //         pointImage.src = `https://www.basketball-reference.com/req/202106291/images/headshots/jordami01.jpg`
+  //         return pointImage
+  //       })
+  //     }
+  //   },
+  //   scales: {
+  //     y: 
+  //       {
+  //         suggestedMin: 0,
+  //         suggestedMax: 80,
+  //         grid: {
+  //           color: '#ffc0cb',
+  //           tickColor: '#ffc0cb',
+  //           drawTicks: true,
+  //         },
+  //         ticks: {
+  //           backdropColor: '#ffc0cb'
+  //         }
+  //       },
+  //     x: {
+  //       grid: {
+  //         color: '#ffc0cb'
+  //       }
+  //     }
+  //   }
+  // })
   
-  const [chartData, setChartData] = useState({
-    labels: Data[team].map((data) => `${data.player.first_name}  ${data.player.last_name}`),
-    datasets: [
-      {
-        label: `Season high scoring vs the ${team}`,
-        data: Data[team].map((data) => data.pts),
-        showLine: false,
-        backgroundColor: 'pink',
-        color: 'green',
-      }
-    ]
-  })
+  // const [chartData, setChartData] = useState({
+  //   labels: Data[team].map((data) => `${data.player.first_name}  ${data.player.last_name}`),
+  //   datasets: [
+  //     {
+  //       label: `Season high scoring vs the ${team}`,
+  //       data: Data[team].map((data) => data.pts),
+  //       showLine: false,
+  //       backgroundColor: 'pink',
+  //       color: 'green',
+  //     }
+  //   ]
+  // })
 
   return (
-    <LineChart chartData={chartData} options={options}/>
+    // <LineChart chartData={chartData} options={options}/>
+    <Teams handleTeams={ handleTeams } teams={ teams } />
   )
 }
 
--- client/Season High/src/components/Teams.jsx
+++ client/Season High/src/components/Teams.jsx
@@ -1,10 +1,45 @@
-import { Data } from '../utils/Data'
 
-const Teams = ({Teams}) => {
+const Teams = ({ handleTeams, teams }) => {
+
+  const teamNames = [
+    "Atlanta Hawks", 
+    "Boston Celtics", 
+    "Brooklyn Nets",
+    "Charlotte Bobcats",
+    "Chicago Bulls", 
+    "Cleveland Cavaliers", 
+    "Dallas Mavericks", 
+    "Denver Nuggets", 
+    "Detroit Pistons", 
+    "Golden State Warriors", 
+    "Houston Rockets", 
+    "Indiana Pacers", 
+    "LA Clippers", 
+    "LA Lakers", 
+    "Memphis Grizzlies", 
+    "Miami Heat", 
+    "Milwaukee Bucks", 
+    "Minnesota Timberwolves", 
+    "New Orleans Hornets", 
+    "New York Knicks", 
+    "Oklahoma City Thunder", 
+    "Orlando Magic", 
+    "Philadelphia Sixers", 
+    "Phoenix Suns", 
+    "Portland Trail Blazers", 
+    "Sacramento Kings", 
+    "San Antonio Spurs", 
+    "Toronto Raptors", 
+    "Utah Jazz", 
+    "Washington Wizards"
+  ]
     return (
       <>
-        {team.map((x) => 
-        <h1>{x.id}</h1>
+        {teamNames.map((team, i) => 
+          <div 
+            key={i}
+            onClick={(e) => handleTeams(team)}
+          >{team}</div>
         )}
       </>
     )
