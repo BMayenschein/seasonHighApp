32a8e403b480fb833143ab1ef402c295c68d63bc
Bruce Mayenschein <brucedmayenschein@gmail.com>
2024-02-20 12:05:22 -0500

    'About' component created

---
+++ client/Season High/src/components/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <div className="text-center">
+            <h1 className="text-5xl font-bold py-6">NBA Season Highs</h1>
+            <p className="">Choose a team from the menu and a chart with all player's season high scoring vs that team will be generated</p>
+            <span>Stats from <a className="link-hover" href="https://github.com/swar/nba_api" target="_blank">nba_api</a></span>
+        </div>
+
+    )
+  }
+  
+  export default About
