5cca8f63253e251c273484423b80b29c02de8bdc
BMayenschein <brucedmayenschein@gmail.com>
2024-04-10 11:11:25 -0400

    using primary theme color for about section

--- client/Season High/src/components/About.jsx
+++ client/Season High/src/components/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
     return (
-        <div className="text-center">
+        <div className="text-center bg-primary">
             <h1 className="text-5xl font-bold p-6">NBA Season Highs</h1>
             <p className="p-6">Choose a team from the menu and a chart with all player's season high scoring vs that team will be generated. (stats from <a className="link-hover" href="https://github.com/swar/nba_api" target="_blank">nba_api</a>)</p>
         </div>
