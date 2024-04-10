03fa266a803268468af49c171bfbac429edbbd57
BMayenschein <brucedmayenschein@gmail.com>
2024-04-10 11:10:40 -0400

    removed excess themes. added winter theme

--- client/Season High/tailwind.config.js
+++ client/Season High/tailwind.config.js
@@ -9,7 +9,7 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "retro", "lemonade", "dim", "nord"], 
+    themes: ["light", "dark", "winter", "nord"], 
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
